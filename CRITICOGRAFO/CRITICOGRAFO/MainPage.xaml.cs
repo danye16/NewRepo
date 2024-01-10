c87c0573fd5dc6d858f74b6c223efa03e99c5814
LDVP5049 <luismaster752@gmail.com>
2024-01-10 09:32:15 -0700

    app de personalidad

--- CRITICOGRAFO/CRITICOGRAFO/MainPage.xaml.cs
+++ CRITICOGRAFO/CRITICOGRAFO/MainPage.xaml.cs
@@ -14,5 +14,111 @@ namespace CRITICOGRAFO
         {
             InitializeComponent();
         }
+
+        private void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e) //Alto
+        {
+        }
+       string genero;
+        string Palabra;
+        
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string nombre = Apodo.Text;
+            if (Hombre.IsChecked)
+            {
+                genero = "MACHO";
+            }
+            if (Mujer.IsChecked)
+            {
+                genero = "Mujer";
+            }
+
+            
+            if (genero=="MACHO")
+            {
+                if (Alto.IsChecked)
+                {
+                    Palabra = "Alto";
+                }
+                if (Feo.IsChecked)
+                {
+                    Palabra += "Feo";
+                }
+                if (Listo.IsChecked)
+                {
+                    Palabra += "Listo";
+                }
+                if (Extravagante.IsChecked)
+                {
+                    Palabra += "Extravagante";
+                }
+                if (Raro.IsChecked)
+                {
+                    Palabra += "Raro";
+                }
+                if (Grande.IsChecked)
+                {
+                    Palabra += "Grande";
+                }
+            }
+            if (genero == "Mujer")
+            {
+                if (Alto.IsChecked)
+                {
+                    Palabra = "Alta";
+                }
+                if (Feo.IsChecked)
+                {
+                    Palabra += "Fea";
+                }
+                if (Listo.IsChecked)
+                {
+                    Palabra += "Lista";
+                }
+                if (Extravagante.IsChecked)
+                {
+                    Palabra += "Extravaganta";
+                }
+                if (Raro.IsChecked)
+                {
+                    Palabra += "Rara";
+                }
+                if (Grande.IsChecked)
+                {
+                    Palabra += "Granda";
+                }
+            }
+
+            string resultado = nombre + Palabra;
+
+            string[] resultadofinal = resultado.Split(new char[] {})
+
+            Resultado.Text=resultado.ToString();
+        }
+
+        private void CheckBox_CheckedChanged_1(object sender, CheckedChangedEventArgs e)//Feo
+        {
+
+        }
+
+        private void CheckBox_CheckedChanged_2(object sender, CheckedChangedEventArgs e)//Listo
+        {
+
+        }
+
+        private void CheckBox_CheckedChanged_3(object sender, CheckedChangedEventArgs e)//Extravagante
+        {
+
+        }
+
+        private void CheckBox_CheckedChanged_4(object sender, CheckedChangedEventArgs e)//Raro
+        {
+
+        }
+        
+        private void CheckBox_CheckedChanged_5(object sender, CheckedChangedEventArgs e) //Grande
+        {
+
+        }
     }
 }
